d0af805ec518b8d143eaee5ef29dfebcf705f077
Ryan Tanenholz <44756861+ryantanen@users.noreply.github.com>
2025-05-28 23:07:39 -0400

    Add discord

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,9 +2,18 @@ import legions from "../../assets/legions.png";
 
 function HomePage() {
   return (
-    <div className="w-full mt-4 flex items-center flex-col justify-center">
+    <div className="w-full mt-4 flex items-center flex-col justify-center gap-2">
       <img src={legions}></img>
       <h1>Welcome to Legions</h1>
+      Join our discord:
+      <a
+        href="https://discord.gg/VzcyzyX9Qy"
+        className="btn btn-primary mt-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Join Discord
+      </a>
     </div>
   );
 }
